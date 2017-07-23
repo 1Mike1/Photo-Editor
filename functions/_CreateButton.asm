481b8ef8ac1a049f66dbbbc714a21efb8408fd3b
1Mike1 <ciesielskimichal7@gmail.com>
2017-07-23 17:03:12 +0200

    Initial commit

---
+++ functions/_CreateButton.asm
@@ -0,0 +1,18 @@
+	push dword 0					;LPVOID lpParam 	// pointer to window-creation data
+	push dword 0			;HINSTANCE hInstance,	// handle to application instance
+	push dword 0					;HMENU hMenu,	// handle to menu, or child-window identifier
+	push dword [windowHandle]		;HWND hWndParent,	// handle to parent or owner window
+	push dword 100					;int nHeight,	// window height
+	push dword 100					;int nWidth,	// window width
+	push dword 10					;int y,	// vertical position of window
+	push dword 10					;int x,	// horizontal position of window
+	push dword WS_VISIBLECHILD		;DWORD dwStyle,	// window style
+	push dword buttonText			;LPCTSTR lpWindowName,	// pointer to window name
+	push dword buttonClass			;LPCTSTR lpClassName,	// pointer to registered class name
+	push dword 0					;DWORD dwExStyle,	// extended window style
+	call _CreateWindowExA@48
+	mov dword [buttonHandle], eax
+	
+	;push dword SW_SHOW
+	;push dword [buttonHandle]
+	;call _ShowWindow@8
---
+++ main.asm
@@ -0,0 +1,88 @@
+global _main
+;extern  _GetStdHandle@4
+;extern  _WriteFile@20
+;extern _ReadFile@20
+extern  _ExitProcess@4
+extern _MessageBoxA@16
+
+extern _GetModuleHandleA@4
+extern _RegisterClassExA@4
+extern _LoadIconA@8
+extern _LoadCursorA@8
+extern _CreateWindowExA@48
+
+extern _DefWindowProcA@16
+extern _ShowWindow@8
+extern _UpdateWindow@4
+extern _DestroyWindow@4
+extern _PostQuitMessage@4
+
+extern _GetMessageA@16
+extern _DispatchMessageA@4
+
+section .bss
+	hInstance resb 4
+	mainWindowClassEx resb 12*4
+	mainMessageStruct resb 7*4
+	windowHandle resb 4
+	buttonHandle resb 4
+	buttonMessageStruct resb 7*4
+	
+	handleIn resd 4
+	handleOut resd 4
+	
+	buffer resb 64
+	ulen resb 4
+	number resb 4
+
+section .data
+	CS_VREDRAW  equ 0x0001
+	CS_HREDRAW  equ 0x0002
+	IDI_APPLICATION equ 32512
+	COLOR_WINDOW equ 5
+	IDC_ARROW equ 32512
+
+	MB_OK  equ 0x00000000
+	MB_ICONEXCLAMATION equ 0x00000030
+
+	WS_SYSMENU  equ 0x00080000
+	WS_CHILD equ 0x40000000
+	WS_VISIBLE equ 0x10000000
+	WS_VISIBLECHILD equ 0x50000000
+
+	SW_SHOW  equ 5
+
+	WM_CREATE equ 0x1
+	WM_DESTROY equ 0x2
+	WM_CLOSE  equ 0x0010
+	
+	WM_COMMAND equ 0x0111
+
+	mainWindowClassName db 'mainWindow', 0
+	mainWindowName db 'Photo Editor', 0
+	
+	buttonClass db 'BUTTON', 0
+	buttonText db 'OK', 0
+	
+	messageTitle db 'Tytu³', 0
+	messageText db 'Tekst', 0
+
+section .text
+_main:
+	;GetModuleHandle(lpModuleName)
+	push 0
+	call _GetModuleHandleA@4
+	mov [hInstance], eax
+	
+	;ShowWindow()
+	%include "functions/_RegisterClassEx.asm"
+	%include "functions/_CreateWindowEx.asm"
+	%include "functions/_CreateButton.asm"
+	%include "functions/_MessageLoop.asm"
+
+_exit:
+	;ExitProcess(0)
+	push 0
+	call _ExitProcess@4
+	
+	%include "functions/_windowProcedure.asm"
